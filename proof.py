e4b97be3982b36226a918bc7246fc43ca48da450
M. Sallermann <M.Sallermann@fz-juelich.de>
2018-06-14 19:01:49 +0200

    asd

--- proof.py
+++ proof.py
@@ -9,6 +9,10 @@ bv = np.array(  [[1,0,0],
 basis = np.array([[0,0,0]])
 N = [3,1,1]
 
+#specify outputfile
+outputfile = "proof_output.txt"
+
+
 #Set up a system
 pos = util.setUpLattice(bv, N, basis)
 spins = util.buildSpins(pos)
@@ -17,7 +21,8 @@ spins = util.buildSpins(pos)
 gradBF = util.calculateGradientsBF(pos, spins)
 print("Brute Force")
 print(gradBF)
-
+#Fourier Transformed grads
+FgradBF = np.fft.fftn(gradBF)
 
 #------------------------------
 #Calculate Gradients with FFT
@@ -51,5 +56,31 @@ fmt=np.fft.fft(mt)
 fDt=np.fft.fft(Dt)
 
 print("With Convolutions")
+res=[]
 for i in range(N[0]):
-    print(np.matmul(fDt[i], fmt[0,i]))
+    res.append(np.matmul(fDt[i], fmt[0,i]))
+res = np.array(res)
+
+print(res)
+#-----------------------------------
+# Write some results to file
+#-----------------------------------
+
+with open(outputfile,"w") as f:
+    f.write("-------------------------------------\n")
+    f.write("Bravais Lattice: \n" + str(bv) + "\n")
+    f.write("N = " + str(N) + "\n")
+    f.write("Basis: \n" + str(basis) + "\n")
+    f.write("-------------------------------------\n")
+    f.write("Original magnetization: \n" + str(spins) + "\n")
+    f.write("Padded magnetization: \n" + str(mt)+ "\n")
+    f.write("Fourier Transformed: \n" + str(fmt)+ "\n")
+    f.write("Padded D-Matrices \n" + str(Dt)+ "\n")
+    f.write("-------------------------------------\n")
+    f.write("Brute Force Gradients: \n"+ str(gradBF) + "\n")
+    f.write("Fourier Tansformed BF Gradients: \n" + str(FgradBF)+ "\n")
+    f.write("Result of ptwise multiplication of Dt and mt: \n")
+    f.write(str(res))
+
+
+
